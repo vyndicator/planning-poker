66a67e825edcfbd37a2b74a836f0cc38cec0c1e1
michaeltyca <michaeltyca@proton.me>
2026-06-01 18:13:03 +0200

    feat: add session validation in guard

--- frontend/src/app/features/session/guards/session.guard.ts
+++ frontend/src/app/features/session/guards/session.guard.ts
@@ -1,3 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { catchError, map, of } from 'rxjs';
+import { SessionResourceService } from '../../../api';
 
-export const sessionGuard: CanActivateFn = () => true;
+export const sessionGuard: CanActivateFn = (route) => {
+  const sessionId = route.paramMap.get('id')!;
+  const api = inject(SessionResourceService);
+  const router = inject(Router);
+
+  return api.getSession({ sessionId }).pipe(
+    map(() => true),
+    catchError(() =>
+      of(router.createUrlTree(['/error'], { queryParams: { message: 'Session not found or has ended.' } })),
+    ),
+  );
+};
